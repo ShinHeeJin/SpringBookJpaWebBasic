a8605938d108af3bb9d275b1c4a596fed330e166
ShinHeeJin <sisesu92@gmail.com>
2023-05-20 11:12:17 +0900

    feat : Address Embeddable

--- src/main/java/jpabook/japshop/domain/Member.java
+++ src/main/java/jpabook/japshop/domain/Member.java
@@ -3,10 +3,7 @@ package jpabook.japshop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +18,9 @@ public class Member {
 
     private String name;
 
+    @Embedded
     private Address address;
 
+    @OneToMany(mappedBy = "member") // 연관관계의 거울 ( 읽기 전용 )
     private List<Order> orders = new ArrayList<>();
 }
