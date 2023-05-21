51d8ab10d4adcdb8ebb555e4cc36be9c9f6ffced
ShinHeeJin <sisesu92@gmail.com>
2023-05-22 00:25:10 +0900

    feat : Item Entity Business Logic

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +31,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items") // 실무에서는 ManyToMany를 거의 사용하지 않는다. 관계 테이블에 추가 컬럼이 항상 들어가기 때문.
     private List<Category> categories = new ArrayList<>();
 
+    //==비지니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
